667c0c00d7dc51cbb8b937abb3ca757ee7a82995
Daisy Yi <daisyyi@rocketmail.com>
2023-05-08 21:40:34 -0700

    commit tree c6c138ff6b0532f6bc74b074a70a51fbfa828045

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Dwayne', last_name: "Johnson", grade: 10)
+Student.create!(first_name: 'Dwayne1', last_name: "Johnson2", grade: 12)
+Student.create!(first_name: 'Dwayne3', last_name: "Johnson4", grade: 13)
